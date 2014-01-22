687435a827610c31c0d9fbe0fbd4ec64789e588c
Luis Mancilla Ávila <luis.mancilla.avila@gmail.com>
2014-01-22 18:29:55 -0300

    FIX: activity indicator white

--- FSImageViewer/FSImageView.m
+++ FSImageViewer/FSImageView.m
@@ -70,7 +70,7 @@
         [_scrollView addSubview:imageView];
         _imageView = imageView;
 
-        activityView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+        activityView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
         activityView.frame = CGRectMake((CGRectGetWidth(self.frame) / 2) - 11.0f, CGRectGetHeight(self.frame) / 2, 22.0f, 22.0f);
         activityView.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin;
         [self addSubview:activityView];
